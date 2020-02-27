1476f4fd25418fdb7d06df8f6df09177fd25566a
Matteo Angioni <matteoangioni@gmail.com>
2020-02-27 12:13:05 +0100

    create repository

---
+++ cmake-build-debug/CMakeFiles/MioProgetto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MioProgetto.dir/main.c.o"
+  "MioProgetto"
+  "MioProgetto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MioProgetto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
